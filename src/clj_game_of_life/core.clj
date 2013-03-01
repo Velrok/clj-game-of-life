4919fb6cc7aab73b5eb9904bbb96d1dfc575d499
Waldemar Schwan <waldemar.schwan@adcloud.com>
2013-03-01 23:41:52 +0100

    progress :)

--- src/clj_game_of_life/core.clj
+++ src/clj_game_of_life/core.clj
@@ -1,20 +1,48 @@
 (ns clj-game-of-life.core
   (:use quil.core))
 
-(def dim 30)
-
-(def world
-  (apply vector
-    (map
-      (fn [_]
-        (apply  vector
-                (map (fn [_] (agent 0))
-                (range dim))))
-      (range dim))))
+(def dim 3)
 
 (defn element
   [x y]
   (-> world (nth y) (nth x)))
+
+(defrecord Cell [alive location])
+
+(defn revive [cell]
+  (assoc cell :alive 1))
+
+(defn kill [cell]
+  (assoc cell :alive 0))
+
+(defn surronding [location]
+  (let [[cell-x cell-y] location
+        x (max (- cell-x 1) 0)
+        y (max (- cell-y 1) 0)
+        width  (- (min (+ cell-x 2) dim) x)
+        height (- (min (+ cell-y 2) dim) y)]
+    (for [xi (range width)
+          yi (range height)]
+      [(+ x xi) (+ y yi)])))
+
+(defn count-alive-cells [cells]
+  (reduce 
+    (fn [sum i] (+ sum i)) 0
+    (map #(:alive %)
+         cells)))
+
+(defn switch-alive [cell]
+  (assoc cell
+    :alive (mod (inc (:alive cell))
+                2)))
+
+(def world
+  (apply vector
+    (map  (fn [y]
+            (apply  vector
+                (map (fn [x] (agent (Cell. 0 [x y])))
+                     (range dim))))
+          (range dim))))
 
 (defn setup []
   (smooth)                          ;;Turn on anti-aliasing
